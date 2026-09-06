9fb916394b8d2c7d505dac057a291f9573a2d58f
Girish Bishwanath <gluttony3012@gmail.com>
2026-09-07 00:01:11 +0530

    fix(landing): update feature component import

--- landing/src/pages/Home.jsx
+++ landing/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Hero from "../components/Hero.jsx";
 import PortalCards from "../components/PortalCards.jsx";
-import Features from "../components/Features.jsx";
+import FeatureGrid from "../components/FeatureGrid.jsx";
 import Workflow from "../components/Workflow.jsx";
 import CTA from "../components/CTA.jsx";
 
@@ -9,7 +9,7 @@ const Home = () => {
     <>
       <Hero />
       <PortalCards />
-      <Features />
+      <FeatureGrid />
       <Workflow />
       <CTA />
     </>
